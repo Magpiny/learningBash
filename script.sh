ac87130ed1ba10bfea3b5e888c3312334e444a27
Magpiny <magpinyb@gmail.com>
2020-10-07 03:20:58 +0300

    initial commit

---
+++ script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+clear
+echo "MAGPINY'S SCRIPT"
+echo "Hi $USER!"
+echo
+
+echo "Connected users"
+echo
+w
+echo
+
+echo "Setting two variables"
+
+COLOUR="BLACK"
+VALUE=9
+
+echo "This is a string: $COLOUR"
+echo "And this is a number: $VALUE"
+echo
+
+echo "BACK TO YOU"
+echo
+
+echo  pwd
+
+echo
+
+
+~
+~
+~ 
---
+++ script1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+clear
+echo "MAGPINY'S SCRIPT"
+echo "Hi $USER!"
+echo
+
+echo "Connected users"
+echo
+w
+echo
+
+echo "Setting two variables"
+
+COLOUR="BLACK"
+VALUE=9
+
+echo "This is a string: $COLOUR"
+echo "And this is a number: $VALUE"
+echo
+
+echo
+echo
+
+echo  'Your current working directory is'
+pwd
+
+echo
+echo "Your files and folder"
+ls
+echo "Processes"
+ps
+
+echo
+echo "BACK TO YOU"
+
+~
+
+
+~
+~ 
